4fac5ae8366fd82e98c14d9d2f144ce7073be53f
jeet083 <156431271+jeet083@users.noreply.github.com>
2024-03-28 12:38:46 +0530

    Add files via upload

---
+++ prac_5.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+
+class circle_radius
+{
+  public:
+         float radius;
+}
+void display()
+{
+    cout<<"r="<<r<<endl;
+}
+};
+int main()
+{
+    class circle c;
+    c.display();
+    return 0;
+}
